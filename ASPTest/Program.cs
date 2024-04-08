91f27bb2e9c3e3d7cac9f13872a8e66ff13c3d64
10-13 <103519265+10-13@users.noreply.github.com>
2024-04-08 14:48:12 +0300

    Update Program.cs

--- ASPTest/Program.cs
+++ ASPTest/Program.cs
@@ -58,16 +58,16 @@ eng.Execute(File.ReadAllText("genScript.js"));
 
 ModelAPI.Model = generator.Model;
 
-// добавляем в приложение сервисы Razor Pages
+// Г¤Г®ГЎГ ГўГ«ГїГҐГ¬ Гў ГЇГ°ГЁГ«Г®Г¦ГҐГ­ГЁГҐ Г±ГҐГ°ГўГЁГ±Г» Razor Pages
 builder.Services.AddRazorPages(options =>
 {
-    // отключаем глобально Antiforgery-токен
+    // Г®ГІГЄГ«ГѕГ·Г ГҐГ¬ ГЈГ«Г®ГЎГ Г«ГјГ­Г® Antiforgery-ГІГ®ГЄГҐГ­
     options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
 });
 
 var app = builder.Build();
 
-// добавляем поддержку маршрутизации для Razor Pages
+// Г¤Г®ГЎГ ГўГ«ГїГҐГ¬ ГЇГ®Г¤Г¤ГҐГ°Г¦ГЄГі Г¬Г Г°ГёГ°ГіГІГЁГ§Г Г¶ГЁГЁ Г¤Г«Гї Razor Pages
 app.MapRazorPages();
 
 app.Run();
@@ -77,134 +77,6 @@ app.Run();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-app.Run(async (context) =>
-{
-    var response = context.Response;
-    var request = context.Request;
-
-    PathString pathOuter = context.Request.Path;
-    if (pathOuter.StartsWithSegments("/game")) 
-    {
-        pathOuter.StartsWithSegments("/game", out pathOuter);
-
-        if (pathOuter.StartsWithSegments("/redirect"))
-        {
-            context.Response.Redirect("/game/time?help");
-        }
-        else if (pathOuter.StartsWithSegments("/time"))
-        {
-            pathOuter.StartsWithSegments("/time", out pathOuter);
-
-            if (context.Request.Query.ContainsKey("help"))
-            {
-                context.Response.ContentType = "html";
-                await context.Response.WriteAsync("<h1>/game/time HELP TABLE</h1>");
-                await context.Response.WriteAsync("</br><sapn>Query arguments:</span>");
-                await context.Response.WriteAsync("<table>");
-
-                await context.Response.WriteAsync(String.Format("<tr><td>{0}</td><td>{1}</td></tr>", "mode", "values: \"UTC\",\"SERVER\",\"SERVER-ticks\"</br>[Shows time depends on mode { defaultMode: \"UTC-ticks\";}]"));
-                await context.Response.WriteAsync(String.Format("<tr><td>{0}</td><td>{1}</td></tr>", "help", "values: NO\n</br>[Shows help(this) menu]"));
-
-                await context.Response.WriteAsync("</table>");
-            }
-            else if (context.Request.Query.ContainsKey("mode"))
-            {
-                if (context.Request.Query["mode"] == "UTC")
-                    await context.Response.WriteAsync(DateTime.UtcNow.ToString());
-                else if (context.Request.Query["mode"] == "SERVER")
-                    await context.Response.WriteAsync(DateTime.Now.ToString());
-                else if (context.Request.Query["mode"] == "SERVER-ticks")
-                    await context.Response.WriteAsync(DateTime.Now.Ticks.ToString());
-                else
-                    await context.Response.WriteAsync(DateTime.UtcNow.Ticks.ToString());
-            }
-            else
-            {
-                await context.Response.WriteAsync(DateTime.UtcNow.Ticks.ToString());
-            }
-        }
-        else if(pathOuter.StartsWithSegments("/json"))
-        {
-            pathOuter.StartsWithSegments("/json", out pathOuter);
-
-            if (context.Request.Query.ContainsKey("mode"))
-            {
-                response.Headers.ContentType = "application/json; charset=utf-8";
-                await response.WriteAsync("{'name':'Tom', 'age':37}");
-            }
-            else
-            {
-                KeyValuePair<string, int> tom = new("Tom", 22);
-                await context.Response.WriteAsJsonAsync(tom);
-            }
-        }
-        else if(pathOuter.StartsWithSegments("/user"))
-        {
-            pathOuter.StartsWithSegments("/user", out pathOuter);
-
-            var message = "Некорректные данные";   // содержание сообщения по умолчанию
-            try
-            {
-                // пытаемся получить данные json
-                var person = await request.ReadFromJsonAsync<Person>();
-                if (person != null) // если данные сконвертированы в Person
-                    message = $"Name: {person.name}  Age: {person.age}";
-            }
-            catch { }
-            // отправляем пользователю данные
-            await response.WriteAsJsonAsync(new { text = message });
-        }
-        else if(pathOuter.StartsWithSegments("/index.html"))
-        {
-            response.ContentType = "text/html; charset=utf-8";
-            await response.SendFileAsync("html/index.html");
-        }
-        else
-        {
-            context.Response.StatusCode = 404;
-            await context.Response.WriteAsync("Game.Model RequestError:\nNotFound");
-        }
-    }
-});
-app.Run();
-
-/*
-
-
-    if (request.Path == "/api/user")
-    {
-        var message = "Некорректные данные";   // содержание сообщения по умолчанию
-        try
-        {
-            // пытаемся получить данные json
-            var person = await request.ReadFromJsonAsync<Person>();
-            if (person != null) // если данные сконвертированы в Person
-                message = $"Name: {person.Name}  Age: {person.Age}";
-        }
-        catch { }
-        // отправляем пользователю данные
-        await response.WriteAsJsonAsync(new { text = message });
-    }
-    else
-    {
-        response.ContentType = "text/html; charset=utf-8";
-        await response.SendFileAsync("html/index.html");
-    }
- 
-*/
 
 class Person
 {
